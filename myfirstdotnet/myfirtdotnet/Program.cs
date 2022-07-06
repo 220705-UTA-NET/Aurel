679a69b4013e7091a0d7b45baaed70193d3d6d8a
Aurel Npounengnong <an6308a@american.edu>
2022-07-06 16:09:49 -0400

    whatever commit

---
+++ myfirstdotnet/myfirtdotnet/Program.cs
@@ -0,0 +1,10 @@
+﻿// This is a comment character/symbol
+
+// See https://aka.ms/new-console-template for more information
+
+
+// Console.Writeline is from the system library and allows us to access a line of text. 
+// (______) parameters. we're sending something to the console
+// "denote a string
+// ; end line charcater. 
+Console.WriteLine("Hello, World!");
